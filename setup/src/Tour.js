66db27d96758aa17a8849b9eee338f9f95f09c57
Catrina Friday <cat.friday@gmail.com>
2020-12-30 20:14:58 -0500

    added tour info to tour component

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 
-const Tour = ({tours}) => {
-  return <section>
-    <div className='title'>
-      <h2>our tours</h2>
+const Tour = ({id, image, info, price, name}) => {
+  return <article className='single-tour'>
+    <img src={image} alt={name}/>
+  <footer>
+    <div className='tour-info'>
+      <h4>{name}</h4>
+      <h4 className='tour-price'>${price}</h4>
     </div>
-  </section>
+    <p>{info}</p>
+    <button className='delete-btn'>not interested</button>
+  </footer>
+  </article>
 };
 
 export default Tour;
