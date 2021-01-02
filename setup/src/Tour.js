796dafabb2dad5b56921d76af00734af17d6ea13
Catrina Friday <cat.friday@gmail.com>
2021-01-02 14:55:23 -0500

    added not interested functionality

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({id, image, info, price, name, removeTour}) => {
 
   const [readMore, setReadMore] = useState(false)
 
@@ -13,16 +13,16 @@ const Tour = ({id, image, info, price, name}) => {
       <h4 className='tour-price'>${price}</h4>
     </div>
     <p>{readMore ? 
-      info
-      :
-      `${info.substring(0, 200)}...`}
-      <button onClick={() => setReadMore(!readMore)}>
-        {readMore ? 'show less'
+        info
         :
-        'read more'}
-      </button>
-      </p>
-    <button className='delete-btn'>not interested</button>
+        `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'show less'
+          :
+          'read more'}
+        </button>
+    </p>
+    <button className='delete-btn' onClick={() => removeTour(id)}>not interested</button>
   </footer>
   </article>)
 };
